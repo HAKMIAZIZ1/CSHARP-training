4734b0bf32899bf3cb50aef4e9c5bd8c4b654a85
HAKMIAZIZ1 <AZIZ.RUGBY@GMAIL.COM>
2018-05-16 20:52:45 +0000

    character

---
+++ Datatypes-character boot type/Datatypes-character boot type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatypes_character_boot_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+            char Myvar = 'G';
+            Console.WriteLine("Min char ={0} Max char ={1} My character ={2}",charMin,charMax,Myvar);
+            Console.ReadKey();
+        }
+    }
+}
